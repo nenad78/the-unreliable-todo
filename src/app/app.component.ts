b090770a522ac560c3e23b2ce71aa0ed543b38f4
nenad78 <nenad.milutinovicdev@gmail.com>
2019-05-07 14:20:31 +0200

    final code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { SessionService } from './services/session.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'the-unreliable-todo';
+export class AppComponent implements OnInit {
+
+  constructor(private sessionService: SessionService) { }
+
+  ngOnInit() {
+    this.sessionService.getCurrentSession();
+  }
+
 }
