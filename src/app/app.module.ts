41ab069b6311d081ff2fd547597b22b928530547
nenad78 <nenad.milutinovicdev@gmail.com>
2019-05-07 13:35:56 +0200

    components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SessionsComponent } from './components/sessions/sessions.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SessionsComponent,
+    TodoFormComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
